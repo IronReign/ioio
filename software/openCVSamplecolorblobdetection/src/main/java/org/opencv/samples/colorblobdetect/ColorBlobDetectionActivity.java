203346c4ecda3ea6cfb0570a79d10ca6486f9c1d
Iron Reign <IronReignRobotics@users.noreply.github.com>
2015-06-13 20:34:23 -0500

    Merged ioiosimple into color blob detector
    can track and color and control the ioio at the same time!

--- software/openCVSamplecolorblobdetection/src/main/java/org/opencv/samples/colorblobdetect/ColorBlobDetectionActivity.java
+++ software/openCVSamplecolorblobdetection/src/main/java/org/opencv/samples/colorblobdetect/ColorBlobDetectionActivity.java
@@ -28,9 +28,24 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.View.OnTouchListener;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class ColorBlobDetectionActivity extends Activity implements OnTouchListener, CvCameraViewListener2 {
+import ioio.lib.api.AnalogInput;
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.IOIO;
+import ioio.lib.api.PwmOutput;
+import ioio.lib.api.exception.ConnectionLostException;
+import ioio.lib.util.BaseIOIOLooper;
+import ioio.lib.util.IOIOLooper;
+import ioio.lib.util.android.IOIOActivity;
+
+public class ColorBlobDetectionActivity extends IOIOActivity implements OnTouchListener, CvCameraViewListener2 {
     private static final String  TAG              = "OCVSample::Activity";
+    private TextView textView_;
+    private SeekBar seekBar_;
+    private ToggleButton toggleButton_;
 
 //    private CameraControl mOpenCvCameraView;
     private boolean              mIsColorSelected = false;
@@ -81,6 +96,14 @@ public class ColorBlobDetectionActivity extends Activity implements OnTouchListe
 
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.color_blob_detection_activity_surface_view);
         mOpenCvCameraView.setCvCameraViewListener(this);
+
+        //setContentView(R.layout.main);
+
+        textView_ = (TextView) findViewById(R.id.TextView);
+        seekBar_ = (SeekBar) findViewById(R.id.SeekBar);
+        toggleButton_ = (ToggleButton) findViewById(R.id.ToggleButton);
+
+        enableUi(false);
     }
 
     @Override
@@ -209,4 +232,72 @@ public class ColorBlobDetectionActivity extends Activity implements OnTouchListe
 
         return new Scalar(pointMatRgba.get(0, 0));
     }
+
+
+    //public class IOIOSimpleApp extends IOIOActivity {
+
+        /*@Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.main);
+
+            textView_ = (TextView) findViewById(R.id.TextView);
+            seekBar_ = (SeekBar) findViewById(R.id.SeekBar);
+            toggleButton_ = (ToggleButton) findViewById(R.id.ToggleButton);
+
+            enableUi(false);
+        }*/
+
+        class Looper extends BaseIOIOLooper {
+            private AnalogInput input_;
+            private PwmOutput pwmOutput_;
+            private DigitalOutput led_;
+
+            @Override
+            public void setup() throws ConnectionLostException {
+                led_ = ioio_.openDigitalOutput(IOIO.LED_PIN, true);
+                input_ = ioio_.openAnalogInput(40);
+                pwmOutput_ = ioio_.openPwmOutput(12, 100);
+                enableUi(true);
+            }
+
+            @Override
+            public void loop() throws ConnectionLostException, InterruptedException {
+                setNumber(input_.read());
+                pwmOutput_.setPulseWidth(500 + seekBar_.getProgress() * 2);
+                led_.write(!toggleButton_.isChecked());
+                Thread.sleep(10);
+            }
+
+            @Override
+            public void disconnected() {
+                enableUi(false);
+            }
+        }
+
+        @Override
+        protected IOIOLooper createIOIOLooper() {
+            return new Looper();
+        }
+
+        private void enableUi(final boolean enable) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    seekBar_.setEnabled(enable);
+                    toggleButton_.setEnabled(enable);
+                }
+            });
+        }
+
+        private void setNumber(float f) {
+            final String str = String.format("%.2f", f);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textView_.setText(str);
+                }
+            });
+        }
+    //}
 }
